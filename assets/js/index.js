b612ab2a4da5a639d338b66b1b5ab41391cb2c14
SamirBelkahla <kafka_67@hotmail.fr>
2022-05-16 00:57:10 +0200

    accueil, inscription js

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,18 @@
+const db = localStorage.getItem("data");
+if (!db) {
+    fetch("../../bdd.json", {
+    method: 'GET',
+    headers: {
+        'Accept': 'application/json, text/plain, */*',
+        'Content-type': 'application/json'
+    }
+}).then(async function (response) {
+    const data = await response.json();
+    localStorage.setItem("data", JSON.stringify(data));
+});
+}
+
+
 function inscription() {
     window.location.href = "inscription.html";
     }
@@ -9,3 +24,7 @@ function google() {
 function facebook() {
     window.open("https://www.facebook.com","_blank", "width=600, height=600") ;
     };
+    
+function mdp() {
+    let mdpOublie = prompt("Veuillez renseigner votre adresse e-mail afin de recevoir votre nouveau mot de passe.", "E-Mail");
+}
