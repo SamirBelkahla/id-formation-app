cc379002ee1d8b612fd8c2f4ade25ec11b59e0a4
SamirBelkahla <kafka_67@hotmail.fr>
2022-05-15 20:13:00 +0200

    accueil, inscription css, html et debut js

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,11 @@
-    function inscription() {
-        window.location.href = "inscription.html";
+function inscription() {
+    window.location.href = "inscription.html";
     }
+
+function google() {
+    window.open("https://accounts.google.com","_blank", "width=600, height=600") ;
+    }
+
+function facebook() {
+    window.open("https://www.facebook.com","_blank", "width=600, height=600") ;
+    };
--- assets/js/inscription.js
+++ assets/js/inscription.js
@@ -1,3 +1,12 @@
-    function accueil() {
-        window.location.href = "index.html";
+function accueil() {
+    window.location.href = "index.html";
     }
+
+
+function google() {
+    window.open("https://accounts.google.com","_blank", "width=600, height=600") ;
+    }
+
+function facebook() {
+    window.open("https://www.facebook.com","_blank", "width=600, height=600") ;
+    };
